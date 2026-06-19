d053e25946c5fafa2f8427fe4d85f3367977f7ea
Swayam Gandhi <swayamgandhi6@gmail.com>
2026-06-19 10:37:03 +0530

    added meta pixwl code

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,6 +38,31 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-DFKREJHWR3');
           `}
         </Script>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1042352164918917');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1042352164918917&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         <ScrollToTop />
         <TyrePartners />
         <Navbar />
